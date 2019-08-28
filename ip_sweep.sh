d9ee0c4451dd9c3bd1b7d97b66654b7a91a5cf66
root <root@localhost.localdomain>
2019-08-28 12:17:05 -0500

    add print_usage function

--- ip_sweep.sh
+++ ip_sweep.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# Given the first three octets of an ip address, pings all sequences of
-# addresses
+# Pings a full range of ip addresses
 
-# Ensure user entered an valid ip
+function print_usage()
+{
+    echo "Please enter a valid ip address"
+    echo "Example usage $0 127.0.0"
+}
+
+# validate the entered ip
 if [[ "$1" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
     # ping full range of ip addresses
     for ip in `seq 1 254`; do
         ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
     done
 else
-    echo "Please enter a valid ip address"
-    echo "Example usage $0 127.0.0"
+    print_usage
     exit 1;
 fi
