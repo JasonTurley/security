db065d76caa8f57ee1dd57dc9de8b32861ce2289
root <root@localhost.localdomain>
2019-08-28 12:01:03 -0500

    validate user entered ip addresses

--- ip_sweep.sh
+++ ip_sweep.sh
@@ -2,13 +2,14 @@
 # Given the first three octets of an ip address, pings all sequences of
 # addresses
 
-if [ "$1" == "" ]
-then
-echo "You forgot an ip address!"
-echo "usage: $0 10.2.0"
-
+# Ensure user entered an valid ip
+if [[ "$1" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+    # ping full range of ip addresses
+    for ip in `seq 1 254`; do
+        ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
 else
-for ip in `seq 1 254`; do
-ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-done
+    echo "Please enter a valid ip address"
+    echo "Example usage $0 127.0.0"
+    exit 1;
 fi
