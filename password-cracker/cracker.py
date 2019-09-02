efcb36d772e788b923e0871b257e37ca2cd5a928
JasonTurley <jasonturley0@gmail.com>
2019-09-02 13:08:48 -0500

    fix issue of not resetting file position each iteration

--- password-cracker/cracker.py
+++ password-cracker/cracker.py
@@ -12,11 +12,6 @@ def print_usage() -> None:
     print("Usage: python3 {} <passwords> <users>".format(sys.argv[0]))
 
 
-def trim_newline(s : str) -> str:
-    """Removes the newline from the end of a string
-    """
-    return s[:len(s)-1]
-
 if len(sys.argv) != 3:
     print_usage()
     sys.exit()
@@ -28,17 +23,16 @@ with open(sys.argv[1], "r") as password_file:
         for creds in user_file:
             user, actual_password = creds.split(":")  # ["user", "password"]
 
-            # remove "\n" from actual_password
-            actual_password = trim_newline(actual_password)
-
             # run full dictionary against user password
             for password in password_file:
-                password = trim_newline(password)
-
                 if password == actual_password:
                     total_cracked += 1
-                    print("{user}'s password is {password}!".format(
-                        user=user, password=password))
+
+                    print("{user}'s password is {password}".format(
+                        user=user, password=password), end="")
                     break
 
-        print("Cracked a total of {} passwords".format(total_cracked))
+            # reset offset to beginning of password file
+            password_file.seek(0, 0)
+
+        print("Cracked {} passwords".format(total_cracked))
