8f856737746decc02bce822022819b5511e1ca4e
JasonTurley <jasonturley0@gmail.com>
2019-09-05 14:52:23 -0500

    reduce time complexity by print connected ports instead of adding to list to traverse at end

--- port_scanner.py
+++ port_scanner.py
@@ -16,57 +16,61 @@ def print_start_banner(target : str, start : str, end : str) -> None:
     """
     Prints a pretty banner for starting up script
     """
-    print("-" * 50)
+    print("-" * 60)
     print("Starting scan on ports {0} - {1} target: {2}".format(start, end, target))
     print("Time started: {}".format(datetime.now()))
-    print("-" * 50)
+    print("-" * 60)
 
 
-def print_finish_banner(target : str, open_ports : list) -> None:
+def print_finish_banner(target : str, num : int) -> None:
     """
     Prints a pretty banner for finishing up script
     """
-    print("-" * 50)
+    print()
+    print("-" * 60)
     print("Finished scan on target: {}".format(target))
     print("Time finished: {}".format(datetime.now()))
-    print("{} open port(s)".format(str(len(open_ports))))
-
-    # print all open port numbers
-    if len(open_ports) != 0:
-        print("[", end="")
-
-        for port in open_ports:
-            print(port, end=" ")
-
-        print("]")
-    print("-" * 50)
+    print("{} open port(s)".format(num))
+    print("-" * 60)
 
 
-if len(sys.argv) != 4:
-    print("Usage: python3 {0} <ip> <start port> <end port>".format(sys.argv[0]))
+# Check command line args
+if len(sys.argv) == 2:
+    # Use default port range 0 to 1000
+    start_port = 0
+    end_port = 1000
+
+elif len(sys.argv) == 4:
+    # Use provided port ranges
+    start_port = int(sys.argv[2])
+    end_port = int(sys.argv[3])
+
+else:
+    # Error
+    print("Usage: python3 {0} <ip> [[start port] [end port]]".format(sys.argv[0]))
     sys.exit()
 
 try:
     # Translate hostname to IPv4
     target = socket.gethostbyname(sys.argv[1])
-    start = sys.argv[2]
-    end = sys.argv[3]
 
-    print_start_banner(target, start, end)
-    open_ports = []
+    print_start_banner(target, str(start_port), str(end_port))
+    count = 0
 
     # Begin scanning ports
-    for port in range(int(start), int(end)):
+    for port in range(start_port, end_port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1.)
         err = s.connect_ex((target, port))
 
+        # A return value of 0 indicates open port
         if not err:
-            open_ports.append(port)
+            count += 1
+            print("port {} is open".format(port))
 
         s.close()
 
-    print_finish_banner(target, open_ports)
+    print_finish_banner(target, count)
 
 except KeyboardInterrupt:
     print("Exiting program.")
