097087b89a42e3a6403e8fac562c47cdb4d151f0
JasonTurley <jasonturley0@gmail.com>
2019-10-22 16:24:06 -0500

    add more info to print_usage()

--- password-cracker/cracker.py
+++ password-cracker/cracker.py
@@ -1,40 +1,41 @@
-"""
-cracker.py - a simple dictionary attack to crack user passwords.
+""" cracker.py - a simple password_list attack to crack user passwords.
 """
 import sys
 
 def print_usage() -> None:
     """Prints the correct way to execute the program
     """
-    print("{} - executes a dictionary attack on a set of user passwords."
-        .format(sys.argv[0]))
+    print("Password Cracker - executes a dictionary attack on a set of user credentials.")
+    print("Created 2019 by Jason Turley")
+    print("Github: https://github.com/JasonTurley/password-cracker")
     print()
-    print("Usage: python3 {} <passwords> <users>".format(sys.argv[0]))
+    print("Usage: python3 {} <PASSWORD-LIST> <USER-LIST>".format(sys.argv[0]))
 
 
+# Check user entered correct number of arguments
 if len(sys.argv) != 3:
     print_usage()
     sys.exit()
 
-with open(sys.argv[1], "r") as dictionary:
+
+with open(sys.argv[1], "r") as password_list:
     with open(sys.argv[2], "r") as credentials:
         total_cracked = 0
 
         for creds in credentials:
             user, actual_password = creds.split(":")  # ["user", "password"]
 
-            # run full dictionary against each user password
-            for password in dictionary:
+            # Execute attack
+            for password in password_list:
                 if password == actual_password:
                     total_cracked += 1
-
-                    print("{user}'s password is {password}".format(
-                        user=user, password=password), end="")
+                    print("{}'s password is {}".format(user, password), end="")
                     break
 
-            # reset offset to beginning of password file
-            dictionary.seek(0, 0)
+            # Reset to the beginning of password list
+            password_list.seek(0, 0)
 
+        # Print results
         print()
-        print("-" * 50)
-        print("Cracked {} passwords".format(total_cracked))
+        print("-" * 30)
+        print("Cracked a total of {} passwords".format(total_cracked))
